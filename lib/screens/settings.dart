9a97af90c911f65c90ad22ce8a1fda1b19800b6d
Dominic Ruggiero <dominic@userexe.me>
2024-06-08 17:01:00 -0400

    add TBA & Github links to settings + version bump

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frc_lookup/util/settings.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -21,10 +22,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: FutureBuilder<void>(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        FutureBuilder<void>(
             future: _loadSettings(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -33,73 +36,102 @@ class _SettingsScreenState extends State<SettingsScreen> {
               if (snapshot.hasError) {
                 return Center(child: Text('Error: ${snapshot.error}'));
               }
-              return ListView(
-                children: [
-                  ListTile(
-                    title: const Text('Download Photos'),
-                    trailing: DropdownButton<PhotoDownloadSetting>(
-                      value: Provider.of<Settings>(context).downloadPhotos,
-                      items: PhotoDownloadSetting.values
-                          .map((e) => DropdownMenuItem<PhotoDownloadSetting>(
-                                value: e,
-                                child: Text(e.toString().split('.').last),
-                              ))
-                          .toList(),
-                      onChanged: (value) {
-                        if (value != null) {
-                          Provider.of<Settings>(context, listen: false)
-                              .setDownloadPhotos(value);
-                        }
-                      },
+              return Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: const Text('Download Photos'),
+                      trailing: DropdownButton<PhotoDownloadSetting>(
+                        value: Provider.of<Settings>(context).downloadPhotos,
+                        items: PhotoDownloadSetting.values
+                            .map((e) => DropdownMenuItem<PhotoDownloadSetting>(
+                                  value: e,
+                                  child: Text(e.toString().split('.').last),
+                                ))
+                            .toList(),
+                        onChanged: (value) {
+                          if (value != null) {
+                            Provider.of<Settings>(context, listen: false)
+                                .setDownloadPhotos(value);
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    title: const Text('Photo Year'),
-                    trailing: DropdownButton<int>(
-                      value: Provider.of<Settings>(context).photoYear,
-                      items: List<int>.generate(35, (i) => 2024 - i)
-                          .map((e) => DropdownMenuItem<int>(
-                                value: e,
-                                child: Text(e.toString()),
-                              ))
-                          .toList(),
-                      onChanged: (value) {
-                        if (value != null) {
-                          Provider.of<Settings>(context, listen: false)
-                              .setPhotoYear(value);
-                        }
-                      },
+                    ListTile(
+                      title: const Text('Photo Year'),
+                      trailing: DropdownButton<int>(
+                        value: Provider.of<Settings>(context).photoYear,
+                        items: List<int>.generate(35, (i) => 2024 - i)
+                            .map((e) => DropdownMenuItem<int>(
+                                  value: e,
+                                  child: Text(e.toString()),
+                                ))
+                            .toList(),
+                        onChanged: (value) {
+                          if (value != null) {
+                            Provider.of<Settings>(context, listen: false)
+                                .setPhotoYear(value);
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  Visibility(
-                    visible: Provider.of<Settings>(context).downloadPhotos ==
-                        PhotoDownloadSetting.comps,
-                    child: Column(
-                      children: [
-                        const ListTile(
-                          title: Text(
-                              'Photo Competitions - space seperated event codes'),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            initialValue: Provider.of<Settings>(context)
-                                .photoComps
-                                .join(' '),
-                            onChanged: (value) {
-                              Provider.of<Settings>(context, listen: false)
-                                  .setPhotoComps(value
-                                      .split(' ')
-                                      .map((e) => e.trim())
-                                      .toList());
-                            },
+                    Visibility(
+                      visible: Provider.of<Settings>(context).downloadPhotos ==
+                          PhotoDownloadSetting.comps,
+                      child: Column(
+                        children: [
+                          const ListTile(
+                            title: Text(
+                                'Photo Competitions - space seperated event codes'),
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              initialValue: Provider.of<Settings>(context)
+                                  .photoComps
+                                  .join(' '),
+                              onChanged: (value) {
+                                Provider.of<Settings>(context, listen: false)
+                                    .setPhotoComps(value
+                                        .split(' ')
+                                        .map((e) => e.trim())
+                                        .toList());
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               );
-            }));
+            }),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('powered by '),
+                InkWell(
+                    child: const Text('The Blue Alliance',
+                        style: TextStyle(color: Colors.blue)),
+                    onTap: () => launchUrl(
+                        Uri.parse('https://thebluealliance.com/'),
+                        mode: LaunchMode.inAppBrowserView)),
+                const Text(' - open source on '),
+                InkWell(
+                    child: const Text('GitHub',
+                        style: TextStyle(color: Colors.blue)),
+                    onTap: () => launchUrl(
+                        Uri.parse('https://github.com/mrhappyma/frc-lookup'),
+                        mode: LaunchMode.inAppBrowserView)),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
